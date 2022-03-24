a07e21cea1d5e176cc18f3ee8cc69bf72923501f
Александр <gugugu52019@gamil.com>
2022-03-24 09:17:56 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Studentlnfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentlnfo {
+    public static void main(String[] args) {
+        System.out.println("Aleksandr Vyborov");
+        System.out.println("22.02.1979");
+    }
+}
